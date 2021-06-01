6b43205ba0cdf130ce3a2d67456a01789c782dcd
Henriquess19 <80883225+Henriquess19@users.noreply.github.com>
2021-06-01 17:26:04 +0100

    Update Door.kt

--- Software/Kotlin/Door.kt
+++ Software/Kotlin/Door.kt
@@ -1,45 +1,46 @@
-
-
 object Door{
     const val WrMask = 0x40
-    //const val WrMask = 0x40
+    const val BusyMask = 0x20
+    const val DOutMask = 0x1F
+    const val MaxSpeed = 0x0F
+    
     fun init(){
-        HAL.clrBits(WrMask)      
-
+        HAL.clrBits(WrMask)
     }
 
 /**
  * V0~3 -> 0x0F
- * OpenClose -> 0x10
+ * 
+ * OpenClose ->  OPEN = 0x10 & CLOSE = 0x00
  */
 
     fun open(speed:Int){
         var spd=speed
-        if (speed > 15) spd=15
+        if (spd > MaxSpeed) spd=MaxSpeed
 
-        val x = 0x10 + spd
-        HAL.writeBits(0x1F,x)  /*Dout Mask*/
-        HAL.setBits(WrMask)           /*Wr Mask*/
+        val x = 0x10 + spd              /*Open action + speed*/
+        HAL.writeBits(DOutMask,x)  
+        HAL.setBits(WrMask)           
 
-        while (!HAL.isBit(0x20)/*Busy Mask*/){}
+        while (!HAL.isBit(BusyMask)){} /*Waiting for the busy signal*/
         HAL.clrBits(WrMask)
     }
 
     fun close(speed: Int){
         var spd=speed
-        if (speed > 15) spd=15
+        if (spd > MaxSpeed) spd=MaxSpeed
 
-        val x = 0x00 + spd
-        HAL.writeBits(0x1F,x)   /*Dout Mask*/
-        HAL.setBits(WrMask)            /*Wr Mask*/
+        val x = 0x00 + spd             /*Close action + speed*/
+        HAL.writeBits(DOutMask,x)   
+        HAL.setBits(WrMask)            
 
-        while (!HAL.isBit(0x20)/*Busy Mask*/){} /*W8ing for the busy signal*/
+        while (!HAL.isBit(BusyMask)){} /*Waiting for the busy signal*/
         HAL.clrBits(WrMask)
     }
 
 
     fun isFinished():Boolean{
-        while (HAL.isBit(0x20)/*Busy Mask*/) {
+        while (HAL.isBit(BusyMask)) {
             return false
         }
         return true
@@ -51,7 +52,7 @@ fun main(){
     Door.init()
 
     while (true) {
-        val x = (-100..100).random()
+        val x = (-100..100).random()        /* TestCode to give random number so door open or close with the value of x*/
         println(x)
         if (x <= 0) {
             Door.close(12)
