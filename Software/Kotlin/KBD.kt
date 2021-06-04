802a885a59b3d68034399f02ae0eb7c9c4966e72
Joao2302 <73882045+Joao2302@users.noreply.github.com>
2021-06-04 17:38:14 +0100

    Update KBD.kt

--- Software/Kotlin/KBD.kt
+++ Software/Kotlin/KBD.kt
@@ -2,14 +2,14 @@ import isel.leic.utils.Time
 
 object KBD {
     const val NONE = 0
-    private const val ACKMask = 0x80
-    private const val DValMask = 0x10 //0x80 -> 0x10 is for simulation purposes
-    private const val KeyValue = 0x0F
+    private const val ACK_MASK = 0x80
+    private const val DVAL_MASK = 0x10 //0x80 -> 0x10 is for simulation purposes
+    private const val KEY_VALUE = 0x0F
     private const val SERIAL_INTERFACE = false
     private val KEYBOARD= charArrayOf('1', '4', '7','*','2','5','8','0','3','6','9','#')
 
     fun init() {
-        HAL.clrBits(ACKMask)
+        HAL.clrBits(ACK_MASK)
     }
     fun getKey():Char{
         if(SERIAL_INTERFACE) return getKeySerial()
@@ -23,11 +23,11 @@ object KBD {
 
     private fun getKeyParallel():Char {
         var x:Char = NONE.toChar()
-        if (HAL.isBit(DValMask)) {
-            x=KEYBOARD[HAL.readBits(KeyValue)]
-            HAL.setBits(ACKMask)
-            while (HAL.isBit(DValMask)){} /*Waiting for Dval to be 0*/
-            HAL.clrBits(ACKMask)
+        if (HAL.isBit(DVAL_MASK)) {
+            x=KEYBOARD[HAL.readBits(KEY_VALUE)]
+            HAL.setBits(ACK_MASK)
+            while (HAL.isBit(DVAL_MASK)){} /*Waiting for Dval to be 0*/
+            HAL.clrBits(ACK_MASK)
             return x
         }
         return x
