8f3be4d1e56204c9fb377501d78ee93421b256ff
Joao2302 <73882045+Joao2302@users.noreply.github.com>
2021-06-06 11:21:47 +0100

    Update Door.kt

--- Software/Kotlin/Door.kt
+++ Software/Kotlin/Door.kt
@@ -1,4 +1,6 @@
 object Door{
+    private const val OPEN = 0x10
+    private const val CLOSE = 0x00
     private const val WR_MASK = 0x40
     private const val BUSY_MASK = 0x20
     private const val D_OUT_MASK = 0x1F
@@ -7,7 +9,6 @@ object Door{
     fun init(){
         HAL.clrBits(WR_MASK)
     }
-
     /**
      * V0~3 -> 0x0F
      *
@@ -17,7 +18,7 @@ object Door{
         var spd=speed
         if (spd > MAX_SPEED) spd=MAX_SPEED
 
-        val x = 0x10 + spd              /*Open action + speed*/
+        val x = OPEN + spd              /*Open action + speed*/
         HAL.writeBits(D_OUT_MASK,x)
         HAL.setBits(WR_MASK)
 
@@ -29,7 +30,7 @@ object Door{
         var spd=speed
         if (spd > MAX_SPEED) spd=MAX_SPEED
 
-        val x = 0x00 + spd             /*Close action + speed*/
+        val x = CLOSE + spd             /*Close action + speed*/
         HAL.writeBits(D_OUT_MASK,x)
         HAL.setBits(WR_MASK)
 
