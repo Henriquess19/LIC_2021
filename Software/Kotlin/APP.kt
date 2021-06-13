396d2b1ab05450b1689f411e0b734a06919cf0b5
Henriquess19 <ricardo.freitas.henriques@gmail.com>
2021-06-13 11:31:55 +0100

    .

--- Software/Kotlin/APP.kt
+++ Software/Kotlin/APP.kt
@@ -1,7 +1,8 @@
 import FileAcess.getUser
 import isel.leic.utils.Time
+import java.util.concurrent.TimeUnit
 
-data class Ut(val user:Int ,val pass:Int,val name:String, var acumulateTime:Int, var jOTA :Long,var entryTime:Long)
+data class Ut(val user:Int ,val pass:Int,val name:String, var acumulateTime:Long, var entryTime:Long)
 
 object APP {
     private const val DOOR_OPEN_VELOCITY= 11
@@ -31,36 +32,84 @@ object APP {
             TUI.writeleft("PASS:", writeLine)
             val code = TUI.key(4, false)
             if (code == user.pass) {
-                return doorMovement(user)
+                return doorAction(user)
             }
             lineClear(writeLine)
             TUI.writeleft("PASS ERROR", writeLine)
             Time.sleep(1000)
             lineClear(writeLine)
         }
-        return restart(tentNumb, writeLine)
+        return wrongPass(tentNumb, writeLine)
     }
 
-    fun restart(tentNumb:Int, writeLine: Int){
+    fun wrongPass(tentNumb:Int, writeLine: Int){
         return pass(tentNumb, writeLine)
     }
 
-    fun doorMovement(worker:Ut){
+    fun doorAction(worker:Ut){
             LCD.clear()
-            TUI.writecenter("Welcome", 0)
-            TUI.writecenter(worker.name, 1)
-            Door.open(DOOR_OPEN_VELOCITY)
-            Time.sleep(3000)
-            Door.close(DOOR_CLOSE_VELOCITY)
-        }
+            if(worker.entryTime > 0L) awayDoor(worker)
+            else entryDoor(worker)
+    }
+
+    fun moveDoor(){
+        Door.open(DOOR_OPEN_VELOCITY)
+        Time.sleep(3000)
+        //Door.close(DOOR_CLOSE_VELOCITY)
+    }
+
+    fun entryDoor(worker:Ut){
+        worker.entryTime = Time.getTimeInMillis()
+        TUI.writecenter("Welcome", 0)
+        TUI.writecenter(worker.name, 1)
+        LogFile.entryUser(worker,worker.entryTime)
+        /**Update User**/
+        moveDoor()
+        resetUserTime(worker)
+    }
+
+    fun awayDoor(worker: Ut){
+        val awayTime = Time.getTimeInMillis()
+        worker.acumulateTime = awayTime - worker.entryTime
+        /*TUI.writeleft(millisToHours(worker.entryTime),0)
+        TUI.writeright(millisToHours(awayTime),0)*/
+        TUI.writecenter(worker.name,0)
+        TUI.writecenter(millisToHours(worker.acumulateTime),1)
+        LogFile.awayUser(worker,worker.entryTime)
+        /**Update User**/
+        moveDoor()
+        resetUserTime(worker)
+    }
+
+    fun millisToHours(millis:Long):String{
+        var time = millis
+        val hour = (time/(60*60*1000))%24
+        time -= hour * (60 * 60 * 1000)
+        val minutes = (time/(60*1000))%60
+        time -= minutes*(60*1000)
+        val seconds = (time/1000)%60
+        return ("${hour}h:${minutes}m:${seconds}s")
+    }
+
+    fun resetUserTime(worker: Ut){
+        worker.entryTime=0L
+    }
 
     fun lineClear(x:Int){                           /* Function to prevent clear all the screen but just one line*/
         TUI.writeleft("                ",x)
     }
 
+    fun restart(tentNumb:Int, writeLine: Int){
+        Time.sleep(5000)
+        LCD.clear()
+        Time.sleep(5000)
+        appPlay(tentNumb, writeLine)
+    }
+
     fun appPlay (tentNumb:Int, writeLine: Int){
         TUI.writeright(TUI.time(),0)
         pass(tentNumb,writeLine)
+        restart(tentNumb,writeLine)
     }
 }
 
--- Software/Kotlin/FileAcess.kt
+++ Software/Kotlin/FileAcess.kt
@@ -1,6 +1,11 @@
 import java.io.File
 
 object FileAcess {
+    private var listUser: Array<String?> = emptyArray()
+
+    init {
+        listUser = read()
+    }
 
     fun read(): Array<String?> {
         val list = File("USERS.txt").inputStream()
@@ -11,14 +16,13 @@ object FileAcess {
 
 
     fun getUser(indiceUser: Int): Ut? {
-        var listUser = read()
         if (listUser[indiceUser] == null || indiceUser == -1){
             return null
         }
         var entry = 0L
         val user = listUser[indiceUser]!!.split(';')
         if (user.lastIndex != listUser[indiceUser]!!.lastIndex) entry = user[user.lastIndex].toLong()
-        return Ut(user[0].toInt(),user[1].toInt(),listUser[indiceUser]!!.split(';')[2],user[3].toInt(),entry,0)
+        return Ut(user[0].toInt(),user[1].toInt(),listUser[indiceUser]!!.split(';')[2],user[3].toLong(),entry)
         TODO("METER GENÉRICO")
     }
 
